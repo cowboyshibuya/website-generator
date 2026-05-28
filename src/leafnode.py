9776c464c454e56feee9b282f67c3df75b50a0cd
cowboyshibuya <cowboyshibuya@pm.me>
2026-05-28 09:19:02 +0200

    add leafnode

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,10 +1,14 @@
 from src.htmlnode import HTMLNode
 
+# a leaf is a HTMLNode class that represents a single HTML tag with no children.
+# for example : <p>Hello World</p> is a leaf.
+# but <p> Hello, <b>World</b> </p> is not a leaf because it contains a children (<b>)
 class LeafNode(HTMLNode):
-    def __init__(self, value, tag):
-        #super().__init__(self)
+    def __init__(self, tag, value, props = {}):
+        super().__init__(self)
         self.value = value
         self.tag = tag
+        self.props = props
 
     # render a leaf node as an HTML string (return string)
     # example:
@@ -15,5 +19,7 @@ class LeafNode(HTMLNode):
             raise ValueError("No value")
         if self.tag == None:
             return self.value
+        return f'<{self.tag}>{self.value}</{self.tag}>'
 
-       return (f"<{self.tag}>{self.value}</{self.tag}>")
+    def __repr__(self):
+        return f"LeafNode : {self.tag} {self.value} {self.props}"
