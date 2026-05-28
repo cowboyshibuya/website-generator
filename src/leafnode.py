2424d3bb072e6f040c7154f4849fb83e01059395
cowboyshibuya <cowboyshibuya@pm.me>
2026-05-28 09:51:09 +0200

    implement parent node

--- src/leafnode.py
+++ src/leafnode.py
@@ -4,20 +4,20 @@ from src.htmlnode import HTMLNode
 # for example : <p>Hello World</p> is a leaf.
 # but <p> Hello, <b>World</b> </p> is not a leaf because it contains a children (<b>)
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value, props = {}):
+    def __init__(self, tag, value, props = None):
         super().__init__(self)
         self.value = value
         self.tag = tag
-        self.props = props
+        self.props = props or {}
 
     # render a leaf node as an HTML string (return string)
     # example:
         # LeafNode("p", "This is a paragraph of text.").to_html()
         # returns -> "<p>This is a paragraph of text.</p>"
     def to_html(self):
-        if self.value == None:
+        if self.value is None:
             raise ValueError("No value")
-        if self.tag == None:
+        if self.tag is None:
             return self.value
         return f'<{self.tag}>{self.value}</{self.tag}>'
 
---
+++ src/parentnode.py
@@ -0,0 +1,20 @@
+from src.htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag = tag, value=None, children=children, props=props)
+        self.tag = tag
+        self.children = children
+        self.props = props or {}
+
+    # we will implement a recursive method to generate each
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("no tag")
+        if self.children is None:
+            raise ValueError("no children")
+
+        # will join all children html method
+        childrenHtml = "".join(child.to_html() for child in self.children)
+
+        return f'<{self.tag}>{childrenHtml}</{self.tag}>'
